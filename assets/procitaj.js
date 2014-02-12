fbeac8cc8a9a713dd0d5159774465b9f255bdba4
ivan marinkovic <marinkovic2006@gmail.com>
2014-02-12 01:23:31 +0100

    sound.js

--- assets/procitaj.js
+++ assets/procitaj.js
@@ -1,41 +1,20 @@
 function ProcitajSad() {
-	var slovoPoSlovo= String(document.getElementById('ovajTekst'));
+   var slovoPoSlovo= String(document.getElementById('ovajTekst'));
 	slovoPoSlovo = slovoPoSlovo.toLowerCase();
 	for (i=0;i<slovoPoSlovo.length;i++) {
-	   switch(slovoPoSlovo.charAt(i))
-		   
-			{
-					case ' ':slovo("assets/ivan/prazno.wav");
-					break;
-					case '1':
-					slovo('assets/ivan/1.wav');
-					setTimeout(function () { 
-    				console.log(1);
-					}, 10000); 
-					break;
-					case '2':slovo('assets/ivan/2.wav');
-					break;
-					case '3':slovo("assets/ivan/3.wav");
-					break;
-					case '4':slovo("assets/ivan/4.wav");
-					break;
-					case '5':slovo("assets/ivan/5.wav");
-					break;
-					case '6':slovo("assets/ivan/6.wav");
-					break;
-					case '7':slovo("assets/ivan/7.wav");
-					break;
-					case '8':slovo("assets/ivan/8.wav");
-					break;
-					case '9':slovo("assets/ivan/9.wav");
-					break;
-					
-					case 'a':slovo("assets/ivan/a.wav");
-					break;
-					case 'b':slovo("assets/ivan/b.wav");
-					break;
-
-					default:slovo("assets/ivan/6.wav");
+        switch(slovoPoSlovo.charAt(i))
+            {
+            case ' ':prazno = new Sound("assets/ivan/prazno.wav");
+              prazno.play();
+            break;
+            case '1': br1 = new Sound("assets/ivan/1.wav");
+              br1.play();
+            break;
+            case '2': br2 = new Sound("assets/ivan/2.wav");
+              br2.play();
+            break;
+            default:ovo = new Sound("assets/ivan/prazno.wav");
+              ovo.play();
 		}
 	}
 }
