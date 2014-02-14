ccab7a366126ea73500b8301f1163f1ec88d3ae9
ivan marinkovic <marinkovic2006@gmail.com>
2014-02-14 22:53:00 +0100

    initialize object removed from loop

--- assets/procitaj.js
+++ assets/procitaj.js
@@ -1,21 +1,21 @@
 function ProcitajSad() {
+    prazno = new Sound("assets/ivan/prazno.wav");
+    br1 = new Sound("assets/ivan/1.wav");
+    br2 = new Sound("assets/ivan/2.wav");
+    ovo = new Sound("assets/ivan/prazno.wav");
    var slovoPoSlovo= String(document.getElementById('ovajTekst'));
 	slovoPoSlovo = slovoPoSlovo.toLowerCase();
 	for (i=0;i<slovoPoSlovo.length;i++) {
         switch(slovoPoSlovo.charAt(i))
             {
-            case ' ':prazno = new Sound("assets/ivan/prazno.wav");
-              prazno.play();
+            case ' ': prazno.play();
             break;
-            case '1': br1 = new Sound("assets/ivan/1.wav");
-              br1.play();
+            case '1': br1.play();
             break;
-            case '2': br2 = new Sound("assets/ivan/2.wav");
-              br2.play();
+            case '2': br2.play();
             break;
-            default:ovo = new Sound("assets/ivan/prazno.wav");
-              ovo.play();
-		}
-	}
+            default: ovo.play();
+		};
+	};
 }
 
